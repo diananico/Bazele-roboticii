881c8e36bc0cee667f8062e9d78323dcc0037ec8
Indreica Claudia-Nicoleta <indreica_nicoleta@yahoo.com>
2018-10-24 11:33:38 -0700

    creare parte grafica

---
+++ ExpandareObstacole/ExpandareObstacole/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace ExpandareObstacole
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnrpct = new System.Windows.Forms.TextBox();
+            this.combouniform = new System.Windows.Forms.ComboBox();
+            this.comboforma = new System.Windows.Forms.ComboBox();
+            this.btnexpand = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtdist1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtdist2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtdist4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtdist3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtdist5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtdist10 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtdist9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtdist8 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtdist7 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtdist6 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.combocapete = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(296, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(514, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numar puncte:";
+            // 
+            // txtnrpct
+            // 
+            this.txtnrpct.Location = new System.Drawing.Point(95, 12);
+            this.txtnrpct.Name = "txtnrpct";
+            this.txtnrpct.Size = new System.Drawing.Size(55, 20);
+            this.txtnrpct.TabIndex = 2;
+            // 
+            // combouniform
+            // 
+            this.combouniform.FormattingEnabled = true;
+            this.combouniform.Items.AddRange(new object[] {
+            "Uniform",
+            "Neuniform"});
+            this.combouniform.Location = new System.Drawing.Point(15, 52);
+            this.combouniform.Name = "combouniform";
+            this.combouniform.Size = new System.Drawing.Size(121, 21);
+            this.combouniform.TabIndex = 3;
+            // 
+            // comboforma
+            // 
+            this.comboforma.FormattingEnabled = true;
+            this.comboforma.Items.AddRange(new object[] {
+            "Cu pastrare forma",
+            "Fara pastrare forma"});
+            this.comboforma.Location = new System.Drawing.Point(12, 147);
+            this.comboforma.Name = "comboforma";
+            this.comboforma.Size = new System.Drawing.Size(121, 21);
+            this.comboforma.TabIndex = 4;
+            // 
+            // btnexpand
+            // 
+            this.btnexpand.Location = new System.Drawing.Point(12, 318);
+            this.btnexpand.Name = "btnexpand";
+            this.btnexpand.Size = new System.Drawing.Size(134, 42);
+            this.btnexpand.TabIndex = 5;
+            this.btnexpand.Text = "Expandeaza";
+            this.btnexpand.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Distanta expandare:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(153, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(99, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // txtdist1
+            // 
+            this.txtdist1.Location = new System.Drawing.Point(153, 107);
+            this.txtdist1.Name = "txtdist1";
+            this.txtdist1.Size = new System.Drawing.Size(55, 20);
+            this.txtdist1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(150, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Distanta 1:";
+            // 
+            // txtdist2
+            // 
+            this.txtdist2.Location = new System.Drawing.Point(153, 150);
+            this.txtdist2.Name = "txtdist2";
+            this.txtdist2.Size = new System.Drawing.Size(55, 20);
+            this.txtdist2.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Distanta 2:";
+            // 
+            // txtdist4
+            // 
+            this.txtdist4.Location = new System.Drawing.Point(153, 242);
+            this.txtdist4.Name = "txtdist4";
+            this.txtdist4.Size = new System.Drawing.Size(55, 20);
+            this.txtdist4.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(150, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Distanta 4:";
+            // 
+            // txtdist3
+            // 
+            this.txtdist3.Location = new System.Drawing.Point(153, 199);
+            this.txtdist3.Name = "txtdist3";
+            this.txtdist3.Size = new System.Drawing.Size(55, 20);
+            this.txtdist3.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(150, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Distanta 3:";
+            // 
+            // txtdist5
+            // 
+            this.txtdist5.Location = new System.Drawing.Point(153, 291);
+            this.txtdist5.Name = "txtdist5";
+            this.txtdist5.Size = new System.Drawing.Size(55, 20);
+            this.txtdist5.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(150, 275);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Distanta 5:";
+            // 
+            // txtdist10
+            // 
+            this.txtdist10.Location = new System.Drawing.Point(229, 291);
+            this.txtdist10.Name = "txtdist10";
+            this.txtdist10.Size = new System.Drawing.Size(55, 20);
+            this.txtdist10.TabIndex = 27;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(226, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 13);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Distanta 10:";
+            // 
+            // txtdist9
+            // 
+            this.txtdist9.Location = new System.Drawing.Point(229, 242);
+            this.txtdist9.Name = "txtdist9";
+            this.txtdist9.Size = new System.Drawing.Size(55, 20);
+            this.txtdist9.TabIndex = 25;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(226, 226);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 13);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Distanta 9:";
+            // 
+            // txtdist8
+            // 
+            this.txtdist8.Location = new System.Drawing.Point(229, 199);
+            this.txtdist8.Name = "txtdist8";
+            this.txtdist8.Size = new System.Drawing.Size(55, 20);
+            this.txtdist8.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(226, 183);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Distanta 8:";
+            // 
+            // txtdist7
+            // 
+            this.txtdist7.Location = new System.Drawing.Point(229, 150);
+            this.txtdist7.Name = "txtdist7";
+            this.txtdist7.Size = new System.Drawing.Size(55, 20);
+            this.txtdist7.TabIndex = 21;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(226, 134);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(58, 13);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Distanta 7:";
+            // 
+            // txtdist6
+            // 
+            this.txtdist6.Location = new System.Drawing.Point(229, 107);
+            this.txtdist6.Name = "txtdist6";
+            this.txtdist6.Size = new System.Drawing.Size(55, 20);
+            this.txtdist6.TabIndex = 19;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(226, 91);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(58, 13);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "Distanta 6:";
+            // 
+            // combocapete
+            // 
+            this.combocapete.FormattingEnabled = true;
+            this.combocapete.Items.AddRange(new object[] {
+            "Arc de cerc",
+            "Taiere"});
+            this.combocapete.Location = new System.Drawing.Point(12, 196);
+            this.combocapete.Name = "combocapete";
+            this.combocapete.Size = new System.Drawing.Size(103, 21);
+            this.combocapete.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 450);
+            this.Controls.Add(this.combocapete);
+            this.Controls.Add(this.txtdist10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtdist9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtdist8);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtdist7);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtdist6);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtdist5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtdist4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtdist3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtdist2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtdist1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnexpand);
+            this.Controls.Add(this.comboforma);
+            this.Controls.Add(this.combouniform);
+            this.Controls.Add(this.txtnrpct);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnrpct;
+        private System.Windows.Forms.ComboBox combouniform;
+        private System.Windows.Forms.ComboBox comboforma;
+        private System.Windows.Forms.Button btnexpand;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtdist1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtdist2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtdist4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtdist3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtdist5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtdist10;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtdist9;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtdist8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtdist7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtdist6;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox combocapete;
+    }
+}
+
